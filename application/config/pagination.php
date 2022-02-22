7334e9f994326a1efac49a2bc16b46d87caecc98
wahyuharis <harits.wahyu.hid@gmail.com>
2022-02-22 10:20:53 +0700

    query string

--- application/config/pagination.php
+++ application/config/pagination.php
@@ -1,6 +1,8 @@
 <?php
     $config['use_page_numbers'] = TRUE;
     $config['page_query_string'] = TRUE;
+    $config['reuse_query_string'] = TRUE;
+    
     $config['query_string_segment'] = 'page';
 
     $config['next_link']        = 'Next';
